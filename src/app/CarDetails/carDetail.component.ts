12a9cffbed50d9a78fdc9bbdbaee1e149c82b750
muhammadfaizannaheem <muhammadfaizannaheem@gmail.com>
2021-08-11 23:33:34 +0500

    2nd Commit

--- src/app/CarDetails/carDetail.component.ts
+++ src/app/CarDetails/carDetail.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-carDetails",
-    templateUrl: "./carDetails.component.html"
+    templateUrl: "./carDetails.component.html",
+    styleUrls: ['./carDetails.component.css']
 })
 export class CarDetailsComponent{
 
