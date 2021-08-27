51d9f109cd193370ecb433c1a2242373c01fea55
muhammadfaizannaheem <muhammadfaizannaheem@gmail.com>
2021-08-27 22:39:57 +0500

    8th commit

--- src/app/CarDetails/carDetail.component.ts
+++ src/app/CarDetails/carDetail.component.ts
@@ -6,13 +6,21 @@ import { Component } from "@angular/core";
     styleUrls: ['./carDetails.component.css']
 })
 export class CarDetailsComponent{
+    CarName : string = '';
     carNumber : string = "JT-327";
     carModel : string = "2005";
     carPrice : string= "15,50,000";
     newCarPrice = "35,50,999";
     getCarPrice()
     {
-       
         return this.newCarPrice;
+    }
+    carCreation : string = 'car not created'; 
+    onCreateCar(){
+        this.carCreation='car creation successfull';
+        return this.carCreation;
+    }
+    onEnterCar(event : Event){
+         this.CarName=(<HTMLInputElement>event.target).value;
     }
 }
